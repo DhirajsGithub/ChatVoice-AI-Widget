71863e0ba60845eeabb03b0a03cade0b4c73935d
Dhiraj Borse <borsedhiraj123@gmail.com>
2025-10-19 15:41:09 +0530

    sm

--- src/constants/avatars.ts
+++ src/constants/avatars.ts
@@ -1,2 +1,2 @@
 // Avatar constants for the Agent Widget
-export const FALLBACK_BOT_AVATAR = 'https://cdn-icons-png.flaticon.com/128/9732/9732800.png';
+export const FALLBACK_BOT_AVATAR = 'https://cdn-icons-png.flaticon.com/128/18355/18355249.png';
